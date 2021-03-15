4862639af0dd92b58254d1fb50d8070491f3fcdb
Alla Kornilova <bukareva90@mail.ru>
2021-03-15 10:51:08 +0300

    fix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -81,7 +81,7 @@ function Cart() {
                <div className="cart__bottom">
                   <div className="cart__bottom-details">
                      <span> Всего пицц: <b>{totalCount} шт.</b> </span>
-                     <span> Сумма заказа: <b>{totalPrice} ₽</b> </span>
+                     <span> Общая сумма заказа: <b>{totalPrice} ₽</b> </span>
                   </div>
                   <div className="cart__bottom-buttons">
                      <Link to="/" className="button button--outline button--add go-back-btn">
